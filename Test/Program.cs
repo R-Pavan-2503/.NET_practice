5602291e0e2f798ec3a1395e4a2bef576fcabc80
Pavan <sara.pavansriram@gmail.com>
2025-11-13 14:56:42 +0530

    nov 13

---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Test
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int i = 0;
+            int j = 9;
+            while (i <= j)
+            {
+                Console.Write($"{arr[j]} ");
+                Console.Write($"{arr[i]} ");
+                i += 1;
+                j -= 1;
+            }
+        }
+    }
+}
---
+++ q1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace q1
+{
+    class A
+    {
+        public static int val = Init("A.val");
+
+
+        static int Init(string s)
+        {
+            Console.WriteLine(s);
+            return 10;
+        }
+    }
+
+    class B : A
+    {
+        public new static int val = Init("B.val");
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(A.val);
+            Console.WriteLine(B.val);
+        }
+    }
+}
